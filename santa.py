192bf911536aaa655afdbea5110f6dd78972eee9
Kaushal Parikh <kaushalp88@gmail.com>
2014-12-13 18:10:02 -0500

    Stylistic updates and code cleanup

--- santa.py
+++ santa.py
@@ -1,7 +1,7 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template
 import os
-import pymongo
 from pymongo import MongoClient
+
 
 app = Flask(__name__)
 uri = os.environ.get('MONGOLAB_URI', 'mongodb://localhost')
@@ -9,34 +9,31 @@ client = MongoClient(uri)
 db = client.heroku_app32469592
 collection = db.santa
 
+
 @app.route('/', methods=['GET'])
 def main_page():
-	print db
-	return render_template('form.html')
+    print db
+    return render_template('form.html')
+
 
 @app.route('/form', methods=['POST'])
 def add_to_database():
-	fname 	= request.form['firstname']	
-	lname 	= request.form['lastname']
-	email 	= request.form['email']
-	info 	= request.form['info']
-	f 	= open('storage.txt', 'a')
-	if (db.santa.find({"email": email}).count() == 0):
-		f.write(fname + "|" + lname + "|" + email + "|" + info + "\n")
-		db.santa.insert({'fname':fname, 'lname':lname, 'email':email, 'info':info})
-	else:
-		return redirect('invalid', 301)	
-	
-	f.close()
-	return redirect('/winner', 301)
+    fname = request.form['firstname']
+    lname = request.form['lastname']
+    email = request.form['email']
+    info = request.form['info']
+    if (db.santa.find({"email": email}).count() == 0):
+        f = open('storage.txt', 'a')
+        f.write(fname + "|" + lname + "|" + email + "|" + info + "\n")
+        f.close()
 
-@app.route('/winner')
-def success():
-	return render_template('worked.html')
+        db.santa.insert({'fname':fname, 'lname':lname, 'email':email, 'info':info})
 
-@app.route('/invalid')
-def invalid():
-	return render_template('invalid.html')
+    else:
+        return render_template('invalid.html')
+
+    return render_template('worked.html')
+
 
 if __name__ == '__main__':
-	app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
